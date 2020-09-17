7d2814f195632cf874d7b52579c0b97838ef7ec4
mean <fixounet@free.fr>
2020-09-17 07:46:40 +0200

    simplify

--- graphics/TFT_eSPI_Custom.h
+++ graphics/TFT_eSPI_Custom.h
@@ -5,8 +5,7 @@
 // This is a generic driver for Arduino boards, it supports SPI interface displays
 // 8 bit parallel interface to TFT is not supported for generic processors
 
-#ifndef _TFT_eSPI_GENERICH_
-#define _TFT_eSPI_GENERICH_
+#pragma once
 
 // Processor ID reported by getSetup()
 #define PROCESSOR_ID 0x0000
@@ -36,27 +35,10 @@
   //#include <FS.h>
 #endif
 
-////////////////////////////////////////////////////////////////////////////////////////
-// Define the DC (TFT Data/Command or Register Select (RS))pin drive code
-////////////////////////////////////////////////////////////////////////////////////////
-#ifndef TFT_DC
-  #define DC_C // No macro allocated so it generates no code
-  #define DC_D // No macro allocated so it generates no code
-#else
   #define DC_C digitalWrite(TFT_DC, LOW)
   #define DC_D digitalWrite(TFT_DC, HIGH)
-#endif
-
-////////////////////////////////////////////////////////////////////////////////////////
-// Define the CS (TFT chip select) pin drive code
-////////////////////////////////////////////////////////////////////////////////////////
-#ifndef TFT_CS
-  #define CS_L // No macro allocated so it generates no code
-  #define CS_H // No macro allocated so it generates no code
-#else
   #define CS_L digitalWrite(TFT_CS, LOW)
   #define CS_H digitalWrite(TFT_CS, HIGH)
-#endif
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // Make sure TFT_RD is defined if not used to avoid an error message
@@ -91,49 +73,10 @@
   #define TFT_MISO -1
 #endif
 
-////////////////////////////////////////////////////////////////////////////////////////
-// Macros to write commands/pixel colour data to an ILI9488 TFT
-////////////////////////////////////////////////////////////////////////////////////////
-#if  defined (ILI9488_DRIVER) // 16 bit colour converted to 3 bytes for 18 bit RGB
 
-  // Write 8 bits to TFT
-  #define tft_Write_8(C)   spi.transfer(C)
-
-  // Convert 16 bit colour to 18 bit and write in 3 bytes
-  #define tft_Write_16(C)  spi.transfer(((C) & 0xF800)>>8); \
-                           spi.transfer(((C) & 0x07E0)>>3); \
-                           spi.transfer(((C) & 0x001F)<<3)
-
-  // Convert swapped byte 16 bit colour to 18 bit and write in 3 bytes
-  #define tft_Write_16S(C) spi.transfer((C) & 0xF8); \
-                           spi.transfer(((C) & 0xE000)>>11 | ((C) & 0x07)<<5); \
-                           spi.transfer(((C) & 0x1F00)>>5)
-  // Write 32 bits to TFT
-  #define tft_Write_32(C)  spi.transfer16((C)>>16); spi.transfer16((uint16_t)(C))
-
-  // Write two address coordinates
-  #define tft_Write_32C(C,D) spi.transfer16(C); spi.transfer16(D)
-
-  // Write same value twice
-  #define tft_Write_32D(C) spi.transfer16(C); spi.transfer16(C)
-
-////////////////////////////////////////////////////////////////////////////////////////
-// Macros to write commands/pixel colour data to other displays
-////////////////////////////////////////////////////////////////////////////////////////
-#else
-  #if  defined (RPI_DISPLAY_TYPE) // RPi TFT type always needs 16 bit transfers
-    #define tft_Write_8(C)     spi.transfer(0); spi.transfer(C)
-  #else
-    #ifdef __AVR__ // AVR processors do not have 16 bit transfer
-      #define tft_Write_8(C)   {SPDR=(C); while (!(SPSR&_BV(SPIF)));}
-      #define tft_Write_16(C)  tft_Write_8((uint8_t)((C)>>8));tft_Write_8((uint8_t)((C)>>0))
-      #define tft_Write_16S(C) tft_Write_8((uint8_t)((C)>>0));tft_Write_8((uint8_t)((C)>>8))
-    #else
       #define tft_Write_8(C)   spi.transfer(C)
       #define tft_Write_16(C)  spi.transfer16(C)
       #define tft_Write_16S(C) spi.transfer16(((C)>>8) | ((C)<<8))
-    #endif // AVR    
-  #endif // RPI_DISPLAY_TYPE
 
   #define tft_Write_32(C) \
   tft_Write_16((uint16_t) ((C)>>16)); \
@@ -146,7 +89,6 @@
   #define tft_Write_32D(C) \
   tft_Write_16((uint16_t) (C)); \
   tft_Write_16((uint16_t) (C))
-#endif
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // Macros to read from display using SPI or software SPI
@@ -161,5 +103,4 @@
   #define tft_Read_8() spi.transfer(0)
 #endif
 
-
-#endif // Header end
+// EOF
